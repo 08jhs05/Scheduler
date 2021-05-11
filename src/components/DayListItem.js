de4f0409add2626ddb7170f21c6bb65b70ae6ede
08jhs05 <08jhs05@gmail.com>
2021-05-11 01:47:42 -0400

    lint codes

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -5,10 +5,13 @@ import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
 
-  const dayListClass = classNames('day-list__item', { '--selected': props.selected }, { '--full': props.spots === 0 })
-
+  const DayListItemClass = classNames('day-list__item', {
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': props.spots === 0
+  });
+  
   return (
-      <li className={dayListClass} onClick={() => props.setDay(props.name)}>
+      <li className={DayListItemClass} onClick={() => props.setDay(props.name)}>
         <h2 className="text--regular">{props.name}</h2> 
         <h3 className="text--light">
           {(props.spots === 0) ? 'no spots' : (props.spots === 1) ? `${props.spots} spot` : `${props.spots} spots`} remaining</h3>
