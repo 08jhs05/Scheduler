d8e9389152578d17787706a93ace0f6657a20997
08jhs05 <08jhs05@gmail.com>
2021-05-11 00:51:28 -0400

    update to show remaining spots with appropriate message

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -9,8 +9,9 @@ export default function DayListItem(props) {
 
   return (
       <li className={dayListClass} onClick={() => props.setDay(props.name)}>
-        <h2 className="text--regular">{ props.name }</h2> 
-        <h3 className="text--light">{ props.spots }</h3>
+        <h2 className="text--regular">{props.name}</h2> 
+        <h3 className="text--light">
+          {(props.spots === 0) ? 'no spots' : (props.spots === 1) ? `${props.spots} spot` : `${props.spots} spots`} remaining</h3>
       </li>
   );
 }
