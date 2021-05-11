f649d23021cb6aedd8d2e0e6cb0a3c3c38f8858d
08jhs05 <08jhs05@gmail.com>
2021-05-11 19:57:47 -0400

    create appointment component

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import DayListItem from './DayListItem'
+import DayListItem from 'components/DayListItem'
 
 export default function Button (props) {
   const dayList = props.days.map( (day) => {
     return <DayListItem 
+    key={day.id}
     name={day.name} 
     spots={day.spots} 
     selected={day.name === props.day}
