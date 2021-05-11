e2c51c332d8f366997072e3e99b6b46ff48458fa
08jhs05 <08jhs05@gmail.com>
2021-05-11 00:33:40 -0400

    lint codes

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,15 @@
-import React from "react";
-import classNames from "classnames";
+import React from 'react'
+import classNames from 'classnames'
 
-import "components/Button.scss";
+import 'components/Button.scss'
 
-export default function Button(props) {
-   const buttonClass = classNames( 'button', { 'button--confirm': props.confirm }, { 'button--danger': props.danger });
- 
-  return <button 
+export default function Button (props) {
+  const buttonClass = classNames('button', { 'button--confirm': props.confirm }, { 'button--danger': props.danger })
+
+  return <button
   onClick={props.onClick}
   disabled={props.disabled}
   className={buttonClass}
   >
-     {props.children}</button>;
+     {props.children}</button>
 }
