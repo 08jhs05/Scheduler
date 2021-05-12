d9cb9381d8aecac67264e178a3981e7be85f1985
08jhs05 <08jhs05@gmail.com>
2021-05-11 22:45:28 -0400

    add appointment components on main application file

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 
 import "components/Application.scss";
-import DayList from "./DayList";
+import DayList from "components/DayList";
+import Appointment from "components/Appointment/index";
 
 const days = [
   {
@@ -21,6 +22,28 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm"
+  },
+];
 
 export default function Application(props) {
   const [dayState, setDayState] = useState("Monday");
@@ -47,7 +70,10 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map( (appointment) => {
+          return <Appointment 
+          key={appointment.id} {...appointment}/>
+        })}
       </section>
     </main>
   );
