128c98234732558af1c7109bb204412f6f314b14
08jhs05 <08jhs05@gmail.com>
2021-05-15 01:02:58 -0400

    add confirm and delete state & delete request with corresponding id when delete icon is clicked

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,6 +5,7 @@ import Show from 'components/Appointment/Show';
 import Empty from 'components/Appointment/Empty';
 import Form from 'components/Appointment/Form';
 import Status from 'components/Appointment/Status';
+import Confirm from './Confirm';
 
 import useVisualMode from 'hooks/useVisualMode';
 
@@ -12,6 +13,8 @@ const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
 const SAVING = "SAVING";
+const DELETING = "DELETING"
+const CONFIRM = "CONFIRM";
 
 export default function Appointment (props) {
 
@@ -30,6 +33,13 @@ export default function Appointment (props) {
     });
   };
 
+  function onDelete() {
+    transition(DELETING);
+    props.cancel(props.id).then( () => {
+      transition(EMPTY);
+    })
+  }
+
   return  <article className="appointment">
     <Header time={props.time}/>
     {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
@@ -37,10 +47,13 @@ export default function Appointment (props) {
       <Show
         student={props.interview.student}
         interviewer={props.interview.interviewer}
+        onDelete={() => transition(CONFIRM)}
       />
     )}
     {mode === CREATE && <Form onSave={save} onCancel={back} interviewers={props.interviewers}/>}
-    {mode === SAVING && <Status/>}
+    {mode === SAVING && <Status message='Saving...'/>}
+    {mode === DELETING && <Status message='Deleting...'/>}
+    {mode === CONFIRM && <Confirm message='Are you sure you would like to delete???' onCancel={() => transition(SHOW)} onConfirm={onDelete}/>}
   </article>;
 }
 
