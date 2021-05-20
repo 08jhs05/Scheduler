0e36d963751769d3da5ab06a19c7cdcb5c31db2d
08jhs05 <08jhs05@gmail.com>
2021-05-20 15:44:28 -0400

    add screenshots

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -34,6 +34,11 @@ export default function Form (props) {
       setError("Student name cannot be blank");
       return;
     }
+
+    if (interviewer === null) {
+      setError("Interviewer must be selected");
+      return;
+    }
   
     setError("");
     props.onSave(name, interviewer);
